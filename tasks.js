db549d65cbd49b1c3faa6dcbe4f56aab233125ce
dinako81 <dina.koliata@gmail.com>
2023-03-03 13:32:28 +0200

    update

--- tasks.js
+++ tasks.js
@@ -109,7 +109,7 @@ console.log('**********10***********')
 // 9. Parašyti funkciją pirminisSkaicius. Funkcija turi vieną kintamąjį. Turi būti patikrinimas, kad kintamasis yra skaičius. Funkcija turi grąžinti ar pateiktas skaičius yra pirminis( pirminis skaičius yra tas, kuris dalinasi tik iš savęs ir tik iš vieneto be liekanos.) (10 taškų)
 
 function pirminisSkaicius(){
-
+   
 }
 console.log('*********************')
 // 10. Parašyti funkciją telefonoNumeris. Funkcija turi priimti vieną kintamąjį - masyvą. Masyvo elementai - skaičiai, ilgis - 10. Funkcija turi grąžinti telefono numerį tokiu formatu
